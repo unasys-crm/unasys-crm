7cb6144a281869e878fc271e03ec545d18303fd2
Sahyadim <edson@smas.com.br>
2025-06-02 22:57:49 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,19 +12,3 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('🚀 API do Unasys CRM funcionando!');
 });
-
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`✅ Servidor rodando na porta ${PORT}`);
-});
-const pool = require('./db');
-
-app.get('/test-db', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT NOW()');
-    res.send(`🗄️ Banco conectado com sucesso: ${result.rows[0].now}`);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Erro ao conectar com o banco');
-  }
-});
