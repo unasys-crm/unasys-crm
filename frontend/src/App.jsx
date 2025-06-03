27b40476990c8cec26db81d2f8083df7614ad2df
Sahyadim <edson@smas.com.br>
2025-06-03 10:43:54 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,3 +14,17 @@ export default App;
 import ModelosMensagens from './pages/ModelosMensagens';
 
 <Route path="/modelos" element={<ModelosMensagens />} />
+
+// src/App.jsx
+import React from 'react';
+import ModelosMensagensList from './components/ModelosMensagensList';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white p-6">
+      <ModelosMensagensList />
+    </div>
+  );
+}
+
+export default App;
