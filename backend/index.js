93370cd0d0fcf28cce12cd09eb211e616bed86a5
Sahyadim <edson@smas.com.br>
2025-06-02 23:01:25 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
@@ -11,4 +10,9 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('🚀 API do Unasys CRM funcionando!');
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`✅ Servidor rodando na porta ${PORT}`);
 });
