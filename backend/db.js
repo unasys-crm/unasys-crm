e7bf1240e8b953fd7a678799fe6424b3141322d7
Sahyadim <edson@smas.com.br>
2025-06-02 22:46:22 -0300

    Create db.js

---
+++ backend/db.js
@@ -0,0 +1,12 @@
+// backend/db.js
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // para ambiente de produção no Render
+  },
+});
+
+module.exports = pool;
