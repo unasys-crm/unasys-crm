a997dbb31c65fb185206bfa1707d2c4b91a39bec
Sahyadim <edson@smas.com.br>
2025-06-07 11:06:08 -0300

    Update AppRoutes.jsx

--- frontend/src/AppRoutes.jsx
+++ frontend/src/AppRoutes.jsx
@@ -1,11 +1,10 @@
-import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import ModelosMensagens from "./pages/ModelosMensagens";
 import RotaPrivada from "./components/RotaPrivada";
 
 export default function AppRoutes() {
-  const isAutenticado = !!localStorage.getItem("usuario");
+  const usuario = JSON.parse(localStorage.getItem("usuario"));
 
   return (
     <BrowserRouter>
@@ -22,28 +21,11 @@ export default function AppRoutes() {
         />
 
         <Route
-          path="/dashboard"
-          element={
-            <RotaPrivada>
-              <div>Dashboard Inicial</div>
-            </RotaPrivada>
-          }
-        />
-
-        {/* Redirecionar raiz para login ou dashboard */}
-        <Route
           path="/"
           element={
-            isAutenticado ? (
-              <Navigate to="/dashboard" replace />
-            ) : (
-              <Navigate to="/login" replace />
-            )
+            usuario ? <Navigate to="/modelos" replace /> : <Navigate to="/login" replace />
           }
         />
-
-        {/* Rota catch-all para rotas não encontradas */}
-        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
