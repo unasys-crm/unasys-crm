e99fad0fa408a332a062108721972d36cc820a7d
Sahyadim <edson@smas.com.br>
2025-06-07 10:32:30 -0300

    Update RotaPrivada.jsx

--- frontend/src/components/RotaPrivada.jsx
+++ frontend/src/components/RotaPrivada.jsx
@@ -1,9 +1,12 @@
 // frontend/src/components/RotaPrivada.jsx
-import React from "react";
 import { Navigate } from "react-router-dom";
 
 export default function RotaPrivada({ children }) {
-  const isAutenticado = !!localStorage.getItem("token");
+  const usuario = JSON.parse(localStorage.getItem("usuario"));
 
-  return isAutenticado ? children : <Navigate to="/login" />;
+  if (!usuario) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
 }
